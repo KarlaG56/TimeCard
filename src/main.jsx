144b81a553603fc283a056eda0e7442ea62c50f1
keyel <crugaldez13@gmail.com>
2024-02-23 05:49:10 -0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
-
+import React from 'react';
+import ReactDOM from 'react-dom'; // Importa desde 'react-dom' en lugar de 'react-dom/client'
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    
   </React.StrictMode>
-)
+);
