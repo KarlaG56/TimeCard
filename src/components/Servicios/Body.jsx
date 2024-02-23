d166e3ed874e72cc4cf0dea9a03262910ac50271
keyel <crugaldez13@gmail.com>
2024-02-23 09:57:47 -0600

    ADD

--- src/components/Servicios/Body.jsx
+++ src/components/Servicios/Body.jsx
@@ -14,16 +14,16 @@ const serviceinf = [
         texto: "Recibe notificaciones personalizadas para recordarte tus sesiones de estudio y tus fechas límite. Nuestros recordatorios te mantienen en el buen camino y te ayudan a mantenerte organizado sin esfuerzo."
     },
     {
-        titulo: "Flashcards Inteligentes",
-        texto: "Crea, estudia y repasa tus tarjetas de estudio de manera fácil y eficiente. Nuestra plataforma te ayuda a recordar lo que necesitas aprender de forma más rápida y sencilla."
+        titulo: "Capacidad de Personalizar las Flashcards",
+        texto: "Personaliza el contenido y el formato de tus flashcards según tus preferencias de estudio y estilo de aprendizaje."
     },
     {
-        titulo: "Calendario Personalizado",
-        texto: "Planifica tus sesiones de estudio y tus tareas de manera simple. Organiza tu tiempo y establece recordatorios para que nunca te pierdas una tarea importante o un momento de estudio."
+        titulo: "Capacidad de Personalizar el Diseño de la Aplicación",
+        texto: "Adapta la interfaz de usuario de la aplicación según tus preferencias de diseño, lo que te permite crear una experiencia de usuario personalizada."
     },
     {
-        titulo: "Recordatorios Automatizados",
-        texto: "Recibe notificaciones personalizadas para recordarte tus sesiones de estudio y tus fechas límite. Nuestros recordatorios te mantienen en el buen camino y te ayudan a mantenerte organizado sin esfuerzo."
+        titulo: "Compartir los Mazos entre la Comunidad",
+        texto: "Colabora con otros usuarios compartiendo tus mazos de flashcards y obteniendo acceso a mazos creados por otros miembros de la comunidad, fomentando un ambiente de aprendizaje colaborativo."
     }
 
 ];
